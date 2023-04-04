4201d1951273efa47dd7e8b4418b65eeae503329
manju007 <127381763+manpi007@users.noreply.github.com>
2023-04-04 12:24:09 +0530

    docker.setup

--- docker.sh
+++ docker.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo docker run hello-world
-# Linux post-install
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl status docker
+sudo apt update
+sudo apt install docker.io -y
+sudo snap install docker
+sudo systemctl status docker
